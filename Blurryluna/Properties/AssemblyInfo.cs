cbe36c9a47383b0056b2b12937f5150f1acedffc
nocturnal <foxnezz@gmail.com>
2019-04-24 20:39:41 +0300

    added assembly info

--- Blurryluna/Properties/AssemblyInfo.cs
+++ Blurryluna/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Blurryluna")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("an instant screenshot tool")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("nocturnal")]
 [assembly: AssemblyProduct("Blurryluna")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
