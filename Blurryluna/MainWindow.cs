fc946c2ac2d5fc8335aef67f9013d32eecda6d14
nocturnal <foxnezz@gmail.com>
2019-04-24 20:34:48 +0300

    added config

--- Blurryluna/MainWindow.cs
+++ Blurryluna/MainWindow.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Blurryluna
@@ -8,9 +9,12 @@ namespace Blurryluna
     public partial class MainWindow : Form
     {
         private static readonly string datetimeFormat = "yyyy-MM-dd HH-mm-ss";
+        private static readonly string configName = "blurryluna.cfg";
+        private static readonly string configSavePath = "savePath=";
 
         private Bitmap screenshot;
         private DateTime screenshotTime;
+        private string screenshotSavePath;
 
         public MainWindow()
         {
@@ -34,8 +38,9 @@ namespace Blurryluna
             var extension = ".png";
 
             var filename = $"{name} {time}{extension}".Trim();
+            var path = Path.Combine(screenshotSavePath, filename);
 
-            screenshot.Save(filename, ImageFormat.Png);
+            screenshot.Save(path, ImageFormat.Png);
         }
 
         private void SaveAndExit()
@@ -48,6 +53,50 @@ namespace Blurryluna
         {
             screenshot = TakeScreenshot();
             screenshotTime = DateTime.Now;
+        }
+
+        private void LoadConfig()
+        {
+            var startupPath = Application.StartupPath;
+            var configPath = Path.Combine(startupPath, configName);
+
+            string message = null;
+            bool badMessageSentiment = true; 
+            if (File.Exists(configPath))
+            {
+                var configRaw = File.ReadAllText(configPath);
+                if (configRaw.StartsWith(configSavePath))
+                {
+                    var loadedSavePath = configRaw.Substring(configSavePath.Length).Trim();
+                    if (Directory.Exists(loadedSavePath))
+                    {
+                        screenshotSavePath = loadedSavePath;
+                    }
+                    else
+                    {
+                        message = "The save path in your config was bad so I've created a new config";
+                    }
+                }
+                else
+                {
+                    message = "Your config format was bad so I've created a new config";
+                }
+            }
+            else
+            {
+                message = "You didn't have a config so I created one";
+                badMessageSentiment = false;
+            }
+
+            if (message != null)
+            {
+                screenshotSavePath = startupPath;
+                var configRaw = $"{configSavePath}{screenshotSavePath}";
+                File.WriteAllText(configPath, configRaw);
+
+                var icon = badMessageSentiment ? MessageBoxIcon.Warning : MessageBoxIcon.Information;
+                MessageBox.Show(message, Application.ProductName, MessageBoxButtons.OK, icon);
+            }
         }
 
         private void Exit()
@@ -68,6 +117,7 @@ namespace Blurryluna
         private void MainWindow_Load(object sender, EventArgs e)
         {
             LoadScreenshot();
+            LoadConfig();
         }
     }
 }
