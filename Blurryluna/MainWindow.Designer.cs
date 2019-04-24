81ee3b6ddc0cf8b2abf463339d481b6280628e7d
nocturnal <foxnezz@gmail.com>
2019-04-24 19:04:13 +0300

    added screenshot taking

---
+++ Blurryluna/MainWindow.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Blurryluna
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameTextbox = new System.Windows.Forms.TextBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameTextbox
+            // 
+            this.nameTextbox.Location = new System.Drawing.Point(12, 58);
+            this.nameTextbox.Name = "nameTextbox";
+            this.nameTextbox.Size = new System.Drawing.Size(246, 20);
+            this.nameTextbox.TabIndex = 0;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(183, 95);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 1;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 130);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.nameTextbox);
+            this.Name = "MainWindow";
+            this.Text = "Blurryluna";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameTextbox;
+        private System.Windows.Forms.Button okButton;
+    }
+}
+
---
+++ Blurryluna/MainWindow.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blurryluna
+{
+    public partial class MainWindow : Form
+    {
+        private Bitmap screenshot;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Bitmap TakeScreenshot()
+        {
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+            var bitmap = new Bitmap(bounds.Width, bounds.Height);
+            var g = Graphics.FromImage(bitmap);
+            g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+
+            return bitmap;
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            var name = nameTextbox.Text;
+
+            if (name == String.Empty)
+            {
+                name = "testy";
+            }
+
+            name += ".png";
+
+            screenshot.Save(name, ImageFormat.Png);
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            screenshot = TakeScreenshot();
+        }
+    }
+}
