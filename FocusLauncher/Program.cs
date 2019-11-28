275ecb8927cc7a755aab0a21ee703254890b9c4b
nocturnal <foxnezz@gmail.com>
2019-11-28 16:05:14 +0300

    added focus launcher

---
+++ FocusLauncher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusLauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process firstProc = new Process();
+            firstProc.StartInfo.FileName = @"R:\my\projects\blurryluna\Blurryluna\bin\Debug\Blurryluna.exe";
+            //firstProc.EnableRaisingEvents = true;
+
+            firstProc.Start();
+
+            //firstProc.WaitForExit();
+        }
+    }
+}
